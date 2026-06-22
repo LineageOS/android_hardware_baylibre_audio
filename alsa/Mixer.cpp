67f4e1431fb1b7347717cf58364e6b807cb756da
Aaron Kling <webgeek1234@gmail.com>
2026-06-22 18:49:32 -0500

    Update to aosp example hal from Android 17
    From hardware/interfaces@45ce123
    
    Change-Id: Ic688462b7d22821ea73c425265ae36a416b47284

--- alsa/Mixer.cpp
+++ alsa/Mixer.cpp
@@ -19,8 +19,8 @@
 #include <fstream>
 
 #define LOG_TAG "AHAL_AlsaMixer"
-#include <android-base/logging.h>
 #include <android-base/properties.h>
+#include <Log.h>
 #include <android/binder_status.h>
 #include <error/expected_utils.h>
 #include <tinyxml2.h>
